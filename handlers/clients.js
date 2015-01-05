598c93231ac374ad1f8e138ee95f536d5cac883c
Juho Vepsalainen <bebraw@gmail.com>
2015-01-05 14:23:06 +0200

    Split up handlers to separate modules

---
+++ handlers/clients.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+module.exports = {
+    get: function(req, res) {
+        res.json({id: 1});
+    },
+    post: function(req, res) {
+        res.json({id: 1});
+    },
+    put: function(req, res) {
+        res.json({id: 1});
+    }
+};
