369c824fd71621421b241bdb66874cb3c8184956
Juho Vepsalainen <bebraw@gmail.com>
2015-01-09 11:03:14 +0200

    Push swagger helpers to an external library

--- spec/definitions/index.js
+++ spec/definitions/index.js
@@ -1,50 +1,5 @@
 'use strict';
-var changeCase = require('change-case');
-var upperCaseFirst = changeCase.upperCaseFirst;
-var sentenceCase = changeCase.sentenceCase;
-
-var fp = require('annofp');
+var swaggerify = require('swaggerify').definitions;
 
 
 module.exports = swaggerify(require('require-dir')());
-
-
-function swaggerify(modules) {
-    var ret = {};
-
-    fp.each(function(module, definition) {
-        var moduleName = upperCaseFirst(module);
-
-        if(definition.index) {
-            fp.each(function(name, def) {
-                var key = name === 'index'? moduleName: moduleName + upperCaseFirst(name);
-
-                if(def.properties) {
-                    def.properties = generateDescriptions(key, def.properties);
-                }
-
-                ret[key] = def;
-            }, definition);
-        }
-        else {
-            ret[moduleName] = definition;
-        }
-    }, modules);
-
-    return ret;
-}
-
-function generateDescriptions(moduleName, properties) {
-    if(!properties) {
-        return properties;
-    }
-
-    return fp.map(function(name, property) {
-        if(!property.description) {
-            property.description = upperCaseFirst(sentenceCase(name)) + ' of the ' +
-                sentenceCase(moduleName);
-        }
-
-        return property;
-    }, properties);
-}
--- spec/paths/index.js
+++ spec/paths/index.js
@@ -1,22 +1,6 @@
 'use strict';
-var upperCaseFirst = require('change-case').upperCaseFirst;
-var fp = require('annofp');
+var swaggerify = require('swaggerify').paths;
 
 
 module.exports = swaggerify(require('require-dir')());
 
-function swaggerify(modules) {
-    var ret = {};
-
-    fp.each(function(name, paths) {
-        ret['/' + name + 's'] = fp.map(function(path, definition) {
-            definition['x-swagger-router-controller'] = name;
-            definition.operationId = path + upperCaseFirst(name);
-            definition.tags = [name];
-
-            return definition;
-        }, paths);
-    }, modules);
-
-    return ret;
-}
